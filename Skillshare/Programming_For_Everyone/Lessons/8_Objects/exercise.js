f4996c2b67b9bcaff80eec8f7998cab9dcaf7a13
Juan Vieira <juan.vieira@parcelpoint.com.au>
2021-08-22 22:24:30 +1000

    objects

---
+++ Skillshare/Programming_For_Everyone/Lessons/8_Objects/exercise.js
@@ -0,0 +1,16 @@
+// create an object 'myAddress' with the values {street: string, number: string, postcode: string, city: string, country: string}
+
+// print myAddress
+
+// print only the value of the street in myAddress using "dot notation" and "brackets notation"
+
+// change the value of the number inside myAddress and print it
+
+// create an array 'countries' which contains 3 countries of your choice
+// each country has the values {name: string, population: number, language: string}
+
+// print the countries array
+
+// print only the countries in the array that speak english
+
+// print only the countries in thea array that the population is greater or equal than 40000
