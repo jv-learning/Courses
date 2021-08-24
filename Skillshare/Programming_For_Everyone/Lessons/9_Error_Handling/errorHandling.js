b4235860e68e0da9448b5325d4c6eaca01b92016
Juan Vieira <juan.vieira@parcelpoint.com.au>
2021-08-24 11:32:54 +1000

    init npm for stackblitz

---
+++ Skillshare/Programming_For_Everyone/Lessons/9_Error_Handling/errorHandling.js
@@ -0,0 +1,60 @@
+// ERROR HANDLING
+// in functions, we should always check that our parameters are valid
+
+// function add(a, b) {
+//   if (typeof a === 'number' && typeof b === 'number') {
+//     return a + b;
+//   }
+//   console.error('This function expects number');
+//   return 0;
+// }
+
+// console.log(add(1, '3'));
+
+function add(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    // prevents unexpected behaviour
+    throw new Error('This function expects number');
+  }
+  return a + b;
+}
+
+// we might want a fallback
+const DEFAULT_RESULT = 30;
+let result = 0;
+
+// try-catch block
+try {
+  // try to do something
+  result = add(10, '10');
+} catch (error) {
+  // if error, get it
+  console.warn(`Error occurred when calling add function: ${error.message}. Setting it to default result ${DEFAULT_RESULT}`);
+  result = DEFAULT_RESULT;
+}
+
+console.log(result);
+
+// prevent program to crash if there is an exception, and we have to manage it on our own
+
+// function upper(text) {
+//   return text.toUpperCase();
+// }
+
+// console.log(upper('hello'));
+// console.log(upper(1234));
+
+function upper(text) {
+  try {
+    return text.toUpperCase();
+  } catch (error) {
+    console.warn(`Error occurred: ${error.message}`);
+    return text;
+  }
+}
+
+console.log(upper('hello'));
+console.log(upper(1234));
+
+// try - catch is mandatory
+// finally is optional. It reads: "regardless if everything was good or we got any errors, execute the code in the finally block"
