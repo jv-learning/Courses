cbac65d41d8813193062cb9e95f581a41d9aef8f
Juan Vieira <juan.vieira@parcelpoint.com.au>
2021-08-29 22:49:33 +1000

    calculator statement

---
+++ Skillshare/Programming_For_Everyone/Lessons/5_MiniProject_Calculator/calculator.js
@@ -0,0 +1,11 @@
+/*
+We want to build a simple calculator
+it accepts two numbers, and an symbol, with indicates the operation
+the permitted symbols are: +, -, *, /. This is a string
+we want to create a function 'calculate(num1, num2, operator)'
+1. checks the operation. is valid?
+  then continue with the program
+  otherwise print a message "The operator {} is not valid" and return null
+2. print the numbers and the operation we are performing
+3. finally, we return the result
+*/
